16f5937179bc64859a726d3c8196e69b18d3295a
cbarillas-2025578 <cbarillas-2025578@kinal.edu.gt>
2026-03-27 14:48:19 -0600

    fix: se arreglo un problema con las referencias del service de detalle venta

--- src/main/java/com/christopherbarillas/KinalApp/repository/DetalleVentaRepository.java
+++ src/main/java/com/christopherbarillas/KinalApp/repository/DetalleVentaRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
+    // Usando la relación @ManyToOne con Venta
     List<DetalleVenta> findByVenta_CodigoVenta(Integer codigoVenta);
+
+    // Usando la relación @ManyToOne con Producto
     List<DetalleVenta> findByProducto_CodigoProducto(Integer codigoProducto);
 }
