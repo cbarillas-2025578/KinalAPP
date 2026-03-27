511b0c548d2ad2078441de4ea0af803b574ca026
cbarillas-2025578 <cbarillas-2025578@kinal.edu.gt>
2026-03-26 19:08:25 -0600

    fix: arreglos en las escritura del repositorio usuario

--- src/main/java/com/christopherbarillas/KinalApp/repository/ProductoRepository.java
+++ src/main/java/com/christopherbarillas/KinalApp/repository/ProductoRepository.java
@@ -3,5 +3,8 @@ package com.christopherbarillas.KinalApp.repository;
 import com.christopherbarillas.KinalApp.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductoRepository extends JpaRepository<Producto,String> {
+    List<Producto> findByEstado(int i);
 }
