785b6ac2196fc3eb57020b7446bd1e81615a3c90
cbarillas-2025578 <cbarillas-2025578@kinal.edu.gt>
2026-04-14 10:40:42 -0600

    fix: se arreglo problemas con el codigo

--- src/main/java/com/christopherbarillas/KinalApp/repository/DetalleVentaRepository.java
+++ src/main/java/com/christopherbarillas/KinalApp/repository/DetalleVentaRepository.java
@@ -2,13 +2,9 @@ package com.christopherbarillas.KinalApp.repository;
 
 import com.christopherbarillas.KinalApp.entity.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
-    // Usando la relación @ManyToOne con Venta
-    List<DetalleVenta> findByVenta_CodigoVenta(Integer codigoVenta);
-
-    // Usando la relación @ManyToOne con Producto
-    List<DetalleVenta> findByProducto_CodigoProducto(Integer codigoProducto);
+@Repository
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+    // El 'Long' de aquí debe coincidir con el tipo de @Id en la entidad
 }
